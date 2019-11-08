f18c1cc098514b6658ad427a4dd6e74999c1c22c
Yogesh Shirsath <yogesh.shirsath@wisdmlabs.com>
2019-11-08 15:24:15 +0530

    Updated compatibility to Moodle3.3

--- version.php
+++ version.php
@@ -26,5 +26,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_disablerightclick';
 $plugin->release = '1.0.0';
 $plugin->version  = 2019110801;
-$plugin->requires = 2016052314;
+$plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
