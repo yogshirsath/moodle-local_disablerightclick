90d30bcf8a1f53bbaaa6526d5ba509ae7af9038e
Yogesh Shirsath <yogesh.shirsath@wisdmlabs.com>
2020-02-24 14:07:55 +0530

    Fixed some more linting errors and added Gruntfile for lint task

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = function (grunt) {
+
+    // We need to include the core Moodle grunt file too, otherwise we can't run tasks like "amd".
+    require("grunt-load-gruntfile")(grunt);
+    // grunt.loadGruntfile("../../Gruntfile.js");
+
+    grunt.initConfig({
+        eslint: {
+            // Even though warnings dont stop the build we don't display warnings by default because
+            // at this moment we've got too many core warnings.
+            options: {quiet: !grunt.option('show-lint-warnings')},
+            amd: {
+                src: 'amd/src/*.js'
+            },
+            // Check YUI module source files.
+        },
+        stylelint: {
+            css: {
+                src: ['styles.css'],
+                options: {
+                    configOverrides: {
+                        rules: {
+                            // These rules have to be disabled in .stylelintrc for scss compat.
+                            "at-rule-no-unknown": true,
+                        }
+                    }
+                }
+            }
+        }
+    });
+    // The default task (running "grunt" in console).
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-stylelint');
+};
